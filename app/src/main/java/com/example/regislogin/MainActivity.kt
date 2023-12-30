2595f4ad22cffa72cea7c09fe064c74653ccbd4a
FajarWnugra <fajarwn474@gmail.com>
2023-12-30 11:15:40 +0700

    done with mainactivity

--- app/src/main/java/com/example/regislogin/MainActivity.kt
+++ app/src/main/java/com/example/regislogin/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.regislogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.regislogin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        binding.btnRegister.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }

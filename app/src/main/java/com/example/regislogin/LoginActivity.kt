5166105e9a8cfdb8f4350b9ea3298f62b7b76e61
FajarWnugra <fajarwn474@gmail.com>
2024-01-03 15:19:17 +0700

    done with validation login/register

--- app/src/main/java/com/example/regislogin/LoginActivity.kt
+++ app/src/main/java/com/example/regislogin/LoginActivity.kt
@@ -1,11 +1,72 @@
 package com.example.regislogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.example.regislogin.databinding.ActivityLoginBinding
+import com.jakewharton.rxbinding2.widget.RxTextView
 
+@Suppress("CheckResult")
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //username validation
+        val usernameStream = RxTextView.textChanges(binding.etEmail)
+            .skipInitialValue()
+            .map { username ->
+                username.isEmpty()
+            }
+        usernameStream.subscribe{
+            showTextMinimalAlert(it, "Username")
+        }
+
+//password validation
+        val passwordStream = RxTextView.textChanges(binding.etPassword)
+            .skipInitialValue()
+            .map { password ->
+                password.isEmpty()
+            }
+        passwordStream.subscribe{
+            showTextMinimalAlert(it, "Password")
+        }
+
+        // Button Enable
+        val invalidFieldStream = io.reactivex.Observable.combineLatest(
+            usernameStream,
+            passwordStream,
+            { usernameInvalid: Boolean, passwordInvalid: Boolean  ->
+                !usernameInvalid && !passwordInvalid
+            })
+        invalidFieldStream.subscribe{ isValid ->
+            if (isValid) {
+                binding.btnLogin.isEnabled = true
+                binding.btnLogin.backgroundTintList = ContextCompat.getColorStateList(this, R.color.primary_color)
+            } else{
+                binding.btnLogin.isEnabled = false
+                binding.btnLogin.backgroundTintList = ContextCompat.getColorStateList(this, android.R.color.darker_gray)
+
+            }
+        }
+
+        binding.btnLogin.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        binding.tvHaventAccount.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+
+    private fun showTextMinimalAlert(isNotValid: Boolean, text: String){
+        if (text == "Email/Username")
+            binding.etEmail.error = if (isNotValid) "$text Tidak boleh Kosong!" else null
+        else if (text == "Password")
+            binding.etPassword.error = if (isNotValid) "$text Tidak Boleh Kosong" else null
     }
 }
